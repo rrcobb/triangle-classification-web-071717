18345bd2e9c6a83a9f605426e78066cf7802dddb
Rob Cobb <rwcobbjr@gmail.com>
2018-03-08 16:43:46 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+class TriangleError < StandardError; end
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  # triangle inequality: a + b > c, a + c > b, b + c > a
+  def triangle_inequality_violated?
+    a + b <= c || b + c <= a || a + c <= b
+  end
+
+  def kind
+    if a <= 0 || b <= 0 || c <= 0 || triangle_inequality_violated?
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif b == c || a == b || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
